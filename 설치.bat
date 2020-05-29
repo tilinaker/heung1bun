438631a0516731769aa6dc63d450d461c9519ea4
ghddbcks <54304944+ghddbcks@users.noreply.github.com>
2020-05-29 13:44:33 +0900

    ㅇ

---
+++ 설치.bat
@@ -0,0 +1,4 @@
+@echo off
+".\Portable python-3.7.3 x64\App\Python\python.exe" -m pip install -r requirements.txt
+pause
+ 
---
+++ 실행.bat
@@ -0,0 +1,3 @@
+@echo off
+".\Portable python-3.7.3 x64\App\Python\python.exe" main.py
+pause
